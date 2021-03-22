1820bbacd23bf6292bbfcf2247b24c2b71dbe691
ndungx <dpron12345@gmail.com>
2021-03-22 20:31:59 +0700

    Update Line_Follower_And_hc_sr04.ino

--- Line_Follower_And_hc_sr04/Line_Follower_And_hc_sr04.ino
+++ Line_Follower_And_hc_sr04/Line_Follower_And_hc_sr04.ino
@@ -9,108 +9,105 @@ int motor_l2 = 5;
 int motor_l1 = 6;
 
 
-int speed = 220;
+int speed = 200;
 
-int frontdist;
+int  frontdist;
 long duration;
 
-int setdist = 10;
+int setdist= 15;
 
 int L_S = A0; //sincer L
 int R_S = A1; //sincer R
 
 
-void setup() {
-  pinMode(motor_l1, OUTPUT);
-  pinMode(motor_l2, OUTPUT);
+void setup(){
+ 
+ pinMode(motor_l1, OUTPUT);
+ pinMode(motor_l2, OUTPUT);
+ 
+ pinMode(motor_r1, OUTPUT);
+ pinMode(motor_r2, OUTPUT);
 
-  pinMode(motor_r1, OUTPUT);
-  pinMode(motor_r2, OUTPUT);
+ pinMode (trigpin, OUTPUT);
+ pinMode (echopin, INPUT);
+ 
+ pinMode(L_S, INPUT);
+ pinMode(R_S, INPUT);
 
-  pinMode (trigpin, OUTPUT);
-  pinMode (echopin, INPUT);
+ Serial.begin(9600);
+ delay(1000);
+ }
 
-  pinMode(L_S, INPUT);
-  pinMode(R_S, INPUT);
+void loop(){
+frontdist = data();
+Serial.println(frontdist); 
 
-  Serial.begin(9600);
-  delay(1000);
+//if (setdist <= 5){stop();}
+
+if(frontdist>setdist){
+if ((digitalRead(L_S) == 1)&&(digitalRead(R_S) == 1)){forword();}
+if ((digitalRead(L_S) == 1)&&(digitalRead(R_S) == 0)){turnRight();}
+if ((digitalRead(L_S) == 0)&&(digitalRead(R_S) == 1)){turnLeft();}
+if ((digitalRead(L_S) == 0)&&(digitalRead(R_S) == 0)){stop();}
+
+}else{
+//backward();
+//delay(500);
+turnLeft();
+delay(300);
+forword();
+delay(800);
+turnRight();  
+delay(300);
+forword();
+delay(800);
+turnRight();
+delay(300);
 }
 
-void loop() {
-  frontdist = data();
-  Serial.println(frontdist);
-
-  if(frontdist > setdist) {
-    if ((digitalRead(L_S) == 1) && (digitalRead(R_S) == 1)) {
-      forword();
-    }
-
-    if ((digitalRead(L_S) == 1) && (digitalRead(R_S) == 0)) {
-      turnRight();
-    }
-
-    if ((digitalRead(L_S) == 0) && (digitalRead(R_S) == 1)) {
-      turnLeft();
-    }
-
-    if ((digitalRead(L_S) == 0) && (digitalRead(R_S) == 0)) {
-      stop();
-    }
-  }
-} else {
-  turnLeft();
-  delay(350);
-  forword();
-  delay(1000);
-  turnRight();
-  delay(200);
-  forword();
-  delay(500);
-  }
 }
 
-long data() {
-  digitalWrite(trigpin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigpin, HIGH);
-  delayMicroseconds(10);
-  duration = pulseIn (echopin, HIGH);
-  return duration / 29 / 2;
+long data(){
+ digitalWrite(trigpin,LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigpin,HIGH);
+ delayMicroseconds(10);
+ duration=pulseIn (echopin,HIGH);
+ return duration / 29 / 2; 
 }
 
-void stop() {
-  analogWrite(motor_l1, 0);
-  analogWrite(motor_l2, 0);
-  analogWrite(motor_r1, 0);
-  analogWrite(motor_r2, 0);
+void stop(){
+ analogWrite(motor_l1, 0);
+ analogWrite(motor_l2, 0);
+ analogWrite(motor_r1, 0);
+ analogWrite(motor_r2, 0);  
 }
 
-void forword() {
-  analogWrite(motor_l1, speed);
-  analogWrite(motor_l2, 0);
-  analogWrite(motor_r1, 0);
-  analogWrite(motor_r2, speed);
+void forword(){
+ analogWrite(motor_l1, speed);
+ analogWrite(motor_l2, 0);
+ analogWrite(motor_r1, 0);
+ analogWrite(motor_r2, speed);  
 }
 
-void backword() {
-  analogWrite(motor_l1, 0);
-  analogWrite(motor_l2, speed);
-  analogWrite(motor_r1, speed);
-  analogWrite(motor_r2, 0);
+void backward(){
+ analogWrite(motor_l1, 0);
+ analogWrite(motor_l2, speed);
+ analogWrite(motor_r1, speed);
+ analogWrite(motor_r2, 0);   
 }
 
 
-void turnRight() {
-  analogWrite(motor_l1, 0);
-  analogWrite(motor_l2, speed);
-  analogWrite(motor_r1, 0);
-  analogWrite(motor_r2, speed);
+void turnRight(){
+analogWrite(motor_l1, 0);
+analogWrite(motor_l2, speed);
+analogWrite(motor_r1, 0);
+analogWrite(motor_r2, speed);  
 }
 
-void turnLeft() {
-  analogWrite(motor_l1, speed);
-  analogWrite(motor_l2, 0);
-  analogWrite(motor_r1, speed);
-  analogWrite(motor_r2, 0);
+void turnLeft(){
+analogWrite(motor_l1, speed);
+analogWrite(motor_l2, 0);
+analogWrite(motor_r1, speed);
+analogWrite(motor_r2, 0);   
 }
